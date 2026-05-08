915fe65fd58401bab44111c44e9488a0479630d5
Bilal <bilaldham1234@gmail.com>
2026-05-08 04:47:19 +0200

    add game page

---
+++ client/app/game/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function GamePage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-bold">
+        🎮 Game Started!
+      </h1>
+    </main>
+  );
+}
